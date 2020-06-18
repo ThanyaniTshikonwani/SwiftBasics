5351e1751c0140dfba7376e404407115162098b7
ThanyaniTshikonwani <thanyani.tshikonwani@umuzi.org>
2020-06-18 15:30:20 +0200

    Third Commit

--- Swift Intro.playground/Contents.swift
+++ Swift Intro.playground/Contents.swift
@@ -72,20 +72,95 @@ struct Person {
     var surname:String
     var age:Int
     
-    
+//    TODO Access
     func fullName(name:String,surname:String) -> String{
         return (name + surname)
     }
+    
     
 }
 
 let person1 = Person(name: "Maria", surname: "Smith", age: 23)
 let person2 = Person(name: "John", surname: "Doe", age: 45)
-
 let person3: Person = Person(name: "Hola", surname: "Jerry", age: 78)
 
-let person4 =  Person()
-per
 print("Profile List  \n\(person1)\n\(person2)\n\(person3)")
 
+class PersonOne {
+    var nameOne: String
+    var surnameOne: String
+    var ageOne:Int!
+    
+    init(nameOne: String,surnameOne: String) {
+        self.nameOne = nameOne
+        self.surnameOne = surnameOne
+        self.ageOne
+        
+    }
+    
+    func printPersonOne(){
+        print("My FullName is \(nameOne)\(surnameOne)")
+    }
+}
+
+let printOne = PersonOne(nameOne: "Deon", surnameOne: "Link")
+
+// TODO Access
+
+// Optional V.S Non-Optional
+var inputString: String = "Hello Africa"
+
+var optionalString: String? = "Hello Africa"
+optionalString = nil
+print(optionalString)
+
+if optionalString != nil{
+    print("The String value wrapped in the optional is: \(optionalString)")
+}
+
+optionalString = "Hello Africa!"
+
+if let unwrappedString = optionalString {
+    print("The value wrapped in the optional string is: \(unwrappedString)")
+}
+
+// Gaurd Let
+
+func unwrap(optionalString: String?){
+    guard let unwrappedString = optionalString else {
+        print("gaurd let returns out of the ")
+        return
+    }
+    print("Now i can use the unwrapped String which has a value of \(unwrappedString)")
+}
+
+unwrap(optionalString: nil)
+unwrap(optionalString: "Hello Africa")
+
+// Enums & Case Statements
+
+enum FontSize {
+    case small,medium,large
+}
+
+let smallFontSze = FontSize.small
+let mediumFontSize = FontSize.medium
+let largeFontSize = FontSize.large
+
+func printFontSize(_ fontSize: FontSize) {
+    
+    switch fontSize {
+    case .small:
+        print("I am small")
+    case .medium:
+        print("I am meduim")
+    case .large:
+        print("I am Large")
+    default:
+        print ("I have no font size")
+    }
+}
+printFontSize(.small)
+printFontSize(.medium)
+printFontSize(.large)
 
